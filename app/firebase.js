7dd15d977edbb953ef32a640599fa75e19f21b53
Suyash Labde <suyashlabde123@gmail.com>
2024-03-17 21:39:45 +0530

    intial commit

--- app/firebase.js
+++ app/firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: YOUR_API_KEY,
-  authDomain: YOUR_AUTH_DOMAIN,
-  projectId: YOUR_PROJECT_ID,
-  storageBucket: YOUR_STORAGE_BUCKET,
-  messagingSenderId: YOU_MESSAGING_SENDER_ID,
-  appId: APP_ID,
+  apiKey: "AIzaSyAuoo7G5gv71k4ruGbO96WN5slMaLFo6-Q",
+  authDomain: "tasktracker-49b12.firebaseapp.com",
+  projectId: "tasktracker-49b12",
+  storageBucket: "tasktracker-49b12.appspot.com",
+  messagingSenderId: "302690121163",
+  appId: "1:302690121163:web:30a958633b7942dc95f24e"
 };
 
 // Initialize Firebase
